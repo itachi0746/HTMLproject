91a282ff06e8ab4a556b36a9f7cdd6f1396a2730
John <601473400@qq.com>
2018-01-11 22:32:13 +0800

    homework1-11

---
+++ 平安普惠/js/index.js
@@ -0,0 +1,6 @@
+function changestyle() {
+    var obj = document.getElementsByClassName('p3-part2')[0],
+        obj2 = document.getElementsByClassName('p3-part3')[0];
+    obj.className = 'btn_go_left';
+    obj2.className = 'btn_go_right';
+}
