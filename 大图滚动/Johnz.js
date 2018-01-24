c037c653d48fe55a5b9637cee78a690c7e136106
John <601473400@qq.com>
2018-01-24 23:25:51 +0800

    1-24

--- 大图滚动/Johnz.js
+++ 大图滚动/Johnz.js
@@ -4,3 +4,8 @@ function $(id) {
 	var ID = document.getElementById(id);
 	return ID;
 }
+
+function $cls(cls) {
+	var result = document.getElementsByClassName(cls);
+	return result;
+}
--- 大图滚动/test.js
+++ 大图滚动/test.js
@@ -1,12 +1,20 @@
-function interval(func, wait){
-  var interv = function(){
-    func.call(null);
-    setTimeout(interv, wait);
-  };
+// function interval(func, wait){
+//   var interv = function(){
+//     func.call(null);
+//     setTimeout(interv, wait);
+//   };
 
-  setTimeout(interv, wait);
-}
+//   setTimeout(interv, wait);
+// }
 
-interval(function(){
-  console.log(2);
-},1000);
+// interval(function(){
+//   console.log(2);
+// },1000);
+
+// var i = 1;
+// var timer = setTimeout(function() {
+//   console.log(i++);
+//   timer = setTimeout(arguments.callee, 2000);
+// }, 2000);
+
+console.log(0%260)
