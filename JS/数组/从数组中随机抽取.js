36934bbff650eec9999a36eb9d93b86bada6742c
John <601473400@qq.com>
2018-01-28 23:18:03 +0800

    1-28

---
+++ JS/数组/从数组中随机抽取.js
@@ -0,0 +1,16 @@
+var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var result = [];
+
+var ranNum = 5;
+
+// for (var i = 0; i < ranNum; i++) {
+
+// 	var ran = Math.floor(Math.random() * arr.length);
+
+// 	result.push(arr.splice(ran, 1)[0]);
+
+// };
+
+// console.log(result);
+console.log(arr.splice(6,1),arr);
