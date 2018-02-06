e0d059a3859d492f1c5d3db47d54c86b35a94a98
John <601473400@qq.com>
2018-02-06 22:53:37 +0800

    2-6

--- Johnz.js
+++ Johnz.js
@@ -11,8 +11,27 @@ function $cls(cls) {
 }
 
 // 产生max,min之间的随机数
-function Random(max,min) {
+function Random(min,max) {
 	max++;
 	var a = Math.floor(Math.random()*(max-min)+min);
 	return a;
+}
+
+// 判断元素是否有某个类
+function hasClass( elements,cName ){ 
+  return !!elements.className.match( new RegExp( "(\\s|^)" + cName + "(\\s|$)") ); 
+}
+
+// 增加类
+function addClass( elements,cName ){ 
+  if( !hasClass( elements,cName ) ){ 
+    elements.className += " " + cName; 
+  }
+}
+
+// 去除类
+function removeClass( elements,cName ){ 
+  if( hasClass( elements,cName ) ){ 
+    elements.className = elements.className.replace( new RegExp( "(\\s|^)" + cName + "(\\s|$)" ), " " );
+  }
 }
--- 全民突击/js/index.js
+++ 全民突击/js/index.js
@@ -6,14 +6,28 @@ var mySwiper = new Swiper('.swiper-container',{
 		}, 
 		slideChangeTransitionEnd: function(){ 
 			swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
-		}
+			if(this.activeIndex==2) {
+				var Dianchi = $('dianchi');
+				console.log(Dianchi.className);
+				setTimeout(function() {
+					Dianchi.className = 'none';
+					console.log(Dianchi.className);
+				},2000)
+			}
+				// console.log(a);
+
+			
+		},
 	},
 
-	direction: 'horizontal',
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
+		direction: 'horizontal',
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
 
 })
+// var a = $('kadun');
+// a.addClass('shake');
+// console.log(mySwiper);
 
