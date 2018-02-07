68feb25e8f7b3294e2f85ea4acf9a750fc83508f
John <601473400@qq.com>
2018-02-07 17:24:29 +0800

    2-7

--- 全民突击/js/index.js
+++ 全民突击/js/index.js
@@ -7,19 +7,43 @@ var mySwiper = new Swiper('.swiper-container',{
 		slideChangeTransitionEnd: function(){ 
 			swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
 			if(this.activeIndex==2) {
-				var Dianchi = $('dianchi');
-				console.log(Dianchi.className);
+				// p3瞄准
+				P3_target.className = 'p3-target';
+				// 电池
 				setTimeout(function() {
-					Dianchi.className = 'none';
-					console.log(Dianchi.className);
-				},2000)
+					Meidian.className = 'meidian2-after';
+				},5000)
+			} else {
+				P3_target.className = 'none';
+				Meidian.className = 'ani infinite meidian2-before';				
 			}
-				// console.log(a);
 
-			
+			if(this.activeIndex==3) {
+				// p4瞄准
+				P4_target.className = 'p4-target';
+				P4_zhujue.className = 'zhujue'
+			}else {
+				P4_target.className = 'none';
+				P4_zhujue.className = 'zhujue-after';
+
+			}
+
+			if(this.activeIndex==4) {
+				// p5瞄准
+				P5_target_one.className = 'ani p5-target-one';
+				P5_target_two.className = 'ani p5-target-two';
+				P5_target_three.className = 'ani p5-target-three';
+				P5_tuanmie.className = 'ani tuanmie';
+			} else {
+				P5_target_one.className = 'ani';
+				P5_target_two.className = 'ani';
+				P5_target_three.className = 'ani';	
+				P5_tuanmie.className = 'ani';
+
+			}
 		},
 	},
-
+		simulateTouch: false,//禁止鼠标模拟
 		direction: 'horizontal',
 		navigation: {
 			nextEl: '.swiper-button-next',
@@ -27,6 +51,16 @@ var mySwiper = new Swiper('.swiper-container',{
 		},
 
 })
+var Meidian = $('meidian2'),
+	P3_target = $('p3-target'),
+	P4_target = $('target'),
+	P4_zhujue = $('zhujue'),
+	P5_target_one = $('target-one'),
+	P5_target_two = $('target-two'),
+	P5_target_three = $('target-three'),
+	P5_tuanmie = $('tuanmie');
+
+
 // var a = $('kadun');
 // a.addClass('shake');
 // console.log(mySwiper);
