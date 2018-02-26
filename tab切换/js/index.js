9c7215b45401a4305910143ac67a19d8e35284e9
John <601473400@qq.com>
2018-02-26 16:35:02 +0800

    2-26

--- tab切换/js/index.js
+++ tab切换/js/index.js
@@ -7,88 +7,88 @@ var Wp = document.getElementById('wp');
 
 
 
-// Btn1.onmouseover = function() {
-// 	// ro();
-// 	// Box1.className = 'db';
+Btn1.onmouseover = function() {
+	// ro();
+	// Box1.className = 'db';
 
-// 	Wp.style.margin = '0 0 0 0%';
-// 	ro_btn();
-// 	Btn1.style.background = '#21b6bb';
-// 	Btn1.style.cursor = 'pointer';
-// }
-// Btn2.onmouseover = function() {
-// 	// ro();
-// 	// Box2.className = 'db';
-// 	Wp.style.margin = '0 0 0 -100%';
+	Wp.style.margin = '0 0 0 0%';
+	ro_btn();
+	Btn1.style.background = '#21b6bb';
+	Btn1.style.cursor = 'pointer';
+}
+Btn2.onmouseover = function() {
+	// ro();
+	// Box2.className = 'db';
+	Wp.style.margin = '0 0 0 -100%';
 
-// 	ro_btn();
+	ro_btn();
 
-// 	Btn2.style.background = '#21b6bb';
-// 	Btn2.style.cursor = 'pointer';
+	Btn2.style.background = '#21b6bb';
+	Btn2.style.cursor = 'pointer';
 
-// }
-// Btn3.onmouseover = function() {
-// 	// ro();
-// 	// Box3.className = 'db';
-// 	Wp.style.margin = '0 0 0 -200%';
+}
+Btn3.onmouseover = function() {
+	// ro();
+	// Box3.className = 'db';
+	Wp.style.margin = '0 0 0 -200%';
 
-// 	ro_btn();
+	ro_btn();
 
-// 	Btn3.style.background = '#21b6bb';
-// 	Btn3.style.cursor = 'pointer';
+	Btn3.style.background = '#21b6bb';
+	Btn3.style.cursor = 'pointer';
 
-// }
-// Btn4.onmouseover = function() {
-// 	// ro();
-// 	// Box4.className = 'db';
-// 	Wp.style.margin = '0 0 0 -300%';
+}
+Btn4.onmouseover = function() {
+	// ro();
+	// Box4.className = 'db';
+	Wp.style.margin = '0 0 0 -300%';
 
-// 	ro_btn();
+	ro_btn();
 
-// 	Btn4.style.background = '#21b6bb';
-// 	Btn4.style.cursor = 'pointer';
+	Btn4.style.background = '#21b6bb';
+	Btn4.style.cursor = 'pointer';
 
-// }
-// Btn5.onmouseover = function() {
-// 	// ro();
-// 	// Box5.className = 'db';
-// 	Wp.style.margin = '0 0 0 -400%';
+}
+Btn5.onmouseover = function() {
+	// ro();
+	// Box5.className = 'db';
+	Wp.style.margin = '0 0 0 -400%';
 
-// 	ro_btn();
-// 	Btn5.style.background = '#21b6bb';
-// 	Btn5.style.cursor = 'pointer';
+	ro_btn();
+	Btn5.style.background = '#21b6bb';
+	Btn5.style.cursor = 'pointer';
 
-// }
-// Btn6.onmouseover = function() {
-// 	// ro();
-// 	// Box6.className = 'db';
-// 	Wp.style.margin = '0 0 0 -500%';
+}
+Btn6.onmouseover = function() {
+	// ro();
+	// Box6.className = 'db';
+	Wp.style.margin = '0 0 0 -500%';
 
-// 	ro_btn();
-// 	Btn6.style.background = '#21b6bb';
-// 	Btn6.style.cursor = 'pointer';
+	ro_btn();
+	Btn6.style.background = '#21b6bb';
+	Btn6.style.cursor = 'pointer';
 
-// }
+}
 
-// function ro() {
-// 	Box1.className = 'dn';
-// 	Box2.className = 'dn';
-// 	Box3.className = 'dn';
-// 	Box4.className = 'dn';
-// 	Box5.className = 'dn';
-// 	Box6.className = 'dn';
+function ro() {
+	Box1.className = 'dn';
+	Box2.className = 'dn';
+	Box3.className = 'dn';
+	Box4.className = 'dn';
+	Box5.className = 'dn';
+	Box6.className = 'dn';
 
-// }
-// function ro_btn() {
-// 	Btn1.style.background = '#5a5a5a';
-// 	Btn2.style.background = '#5a5a5a';
-// 	Btn3.style.background = '#5a5a5a';
-// 	Btn4.style.background = '#5a5a5a';
-// 	Btn5.style.background = '#5a5a5a';
-// 	Btn6.style.background = '#5a5a5a';
+}
+function ro_btn() {
+	Btn1.style.background = '#5a5a5a';
+	Btn2.style.background = '#5a5a5a';
+	Btn3.style.background = '#5a5a5a';
+	Btn4.style.background = '#5a5a5a';
+	Btn5.style.background = '#5a5a5a';
+	Btn6.style.background = '#5a5a5a';
 
-// }
+}
 
-// function fl() {
+function fl() {
 
-// }
+}
