b2d772ab46622e9f496a172d861a66366c1eb15e
John <601473400@qq.com>
2018-02-28 22:41:55 +0800

    2-28

--- work/2.28碰撞检测/test.js
+++ work/2.28碰撞检测/test.js
@@ -1,5 +1,9 @@
-if(!(2>5)){
-	console.log(111);
-}else {
-	console.log(222);
+// if(!(2>5)){
+// 	console.log(111);
+// }else {
+// 	console.log(222);
+// }
+
+if(true&&true) {
+	console.log(333);
 }
