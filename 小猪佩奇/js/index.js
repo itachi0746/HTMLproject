af8341e8786af34e1fb82f7c74a96544e07b615b
John <601473400@qq.com>
2018-03-14 23:24:54 +0800

    3-14

--- 小猪佩奇/js/index.js
+++ 小猪佩奇/js/index.js
@@ -1,123 +1,7 @@
-$(function() {
-	var t = new TimelineMax();
-	var t1 = new TimelineMax();
-	var t2 = new TimelineMax();
-	var t3 = new TimelineMax();
-	var t4 = new TimelineMax();
-	var t5 = new TimelineMax();
-	var t6 = new TimelineMax();
-	var t7 = new TimelineMax();
-	var t8 = new TimelineMax();
-	// 太阳
-	t1.to(sun,10,{
-		rotation:'360deg',
-		ease: Linear.easeIn,
-	});
-	// 猪爸妈前进
-	t.from('#zba',4,{
-		left:'-15rem',
-	})
-	.fromTo(homeStart,1,{
-		opacity: 0,
-	},{
-		opacity: 1,
-	})
-	.from('#zma',4,{left:'-10rem'},0);
 
-	// 猪左脚
-	t3.from('#zba_fl',1,{
-		rotation:'-30deg',
-		ease:Linear.easeIn,
-	})
-	.to('#zba_fl',1,{
-		rotation:'-30deg',
-		ease:Linear.easeIn,
-	})
-	.to('#zma_fl',1,{
-		rotation:'-30deg',
-		ease:Linear.easeIn,
-	},0)
-	.to('#zma_fl',1,{
-		rotation:'0deg',
-		ease:Linear.easeIn,
-	},'-=1');
-
-	// 猪右脚
-	t4.from('#zba_fr',1,{
-		rotation:'30deg',
-		ease:Linear.easeIn,
-	})
-	.to('#zba_fr',1,{
-		rotation:'30deg',
-		ease:Linear.easeIn,
-	})
-	.to('#zma_fr',1,{
-		rotation:'50deg',
-		ease:Linear.easeIn,
-	},0)
-	.to('#zma_fr',1,{
-		rotation:'0deg',
-		ease:Linear.easeIn,
-	},'-=2');
-
-	// 小猪跳跃
-	t5.staggerTo('#zsi',1,{
-		cycle: {
-			bezier: function() {
-				return [
-					{x:0,y:0},
-					{x:50,y:-100},	
-					{x:130,y:0},
-				]
-			}
-		},
-		ease:Ease.easeIn,		
-	})
-	.staggerTo('#zsi',1,{
-		cycle: {
-			bezier: function() {
-				return [
-					{x:130,y:0},
-					{x:210,y:-100},
-					{x:290,y:0},
-				]
-			}
-		},
-		ease:Ease.easeIn,
-	});
-	// 猪弟弟跳跃
-	t6.staggerTo('#zbo',1,{
-		cycle: {
-			bezier: function() {
-				return [
-					{x:0,y:0},
-					{x:100,y:-100},	
-					{x:180,y:0},
-				]
-			}
-		},
-		ease:Ease.easeIn,		
-	})
-	.staggerTo('#zbo',1,{
-		cycle: {
-			bezier: function() {
-				return [
-					{x:180,y:0},
-					{x:260,y:-100},
-					{x:350,y:0},
-				]
-			}
-		},
-		ease:Ease.easeIn,
-	})
-
-	t1.repeat(-1);
-	t2.repeat(1);
-	t3.repeat(1);
-	t4.repeat(1);
-	t7.repeat(1);
-	t8.repeat(1);
-});
+// 计算出根元素的font-size
+var cw = document.documentElement.clientWidth;
+var fs =  40* (cw / 750);
 
 var homeStart = $('#homePageStart');
 var zba = $('#zba');
@@ -126,6 +10,177 @@ var zsi = $('#zsi');
 var zbo = $('#zbo');
 var sun = $('.sun');
 // console.log(sun);
+
+
+// ready事件(首页的动画)
+$(function() {
+	var t = new TimelineMax();
+	var t1 = new TimelineMax();
+	var t2 = new TimelineMax();
+	var t3 = new TimelineMax();
+	var t4 = new TimelineMax();
+	var t5 = new TimelineMax();
+	var t6 = new TimelineMax();
+
+	// 太阳
+	t1.to(sun,10,{
+		rotation:'360deg',
+		ease: Linear.easeIn,
+	});
+	// 猪爸妈前进
+	t.from(zba,4,{
+		x:-15*fs,
+		ease: Linear.easeIn,
+	})
+	.fromTo(homeStart,1,{
+		opacity: 0,
+	},{
+		opacity: 1,
+	})
+	.from(zma,4,{
+		x:-15*fs,
+		ease: Linear.easeIn,
+	},0);
+
+	// 猪姐弟前进
+	t2.from(zsi,4.2,{
+		x:-18*fs,
+		ease: Linear.easeIn,
+	})
+	.from(zbo,4.2,{
+		x:-18*fs,
+		ease: Linear.easeIn,
+	},0);
+
+	// 猪左脚
+	t3.to('#zba_fl',1,{
+		x : 1.5*fs,
+		rotation : '-30deg',
+		ease : Linear.easeIn,
+	})
+	.to('#zba_fl',1,{
+		x : 0,
+		rotation :'0deg',
+		ease :Linear.easeIn,
+	})
+	.to('#zma_fl',1,{
+		x : 1.5*fs,
+		rotation:'-30deg',
+		ease:Linear.easeIn,
+	},0)
+	.to('#zma_fl',1,{
+		x : 0,
+		rotation:'0deg',
+		ease:Linear.easeIn,
+	},'-=1');
+
+	// 猪右脚
+	t4.to('#zba_fr',1,{
+		x : -1.5*fs,
+		rotation : '30deg',
+		ease : Linear.easeIn,
+	})
+	.to('#zba_fr',1,{
+		x : 0,
+		rotation:'0deg',
+		ease:Linear.easeIn,
+	})
+	.to('#zma_fr',1,{
+		x : -1.5*fs,
+		rotation:'30deg',
+		ease:Linear.easeIn,
+	},0)
+	.to('#zma_fr',1,{
+		x : 0,
+		rotation:'0deg',
+		ease:Linear.easeIn,
+	},'-=1');
+
+	// 猪姐姐跳跃,时长1.4s
+	t5.to('#zsi_fl',.2,{
+		x : 1.5*fs,
+		rotation:'-30deg',
+
+	})
+	.to('#zsi_fr',.2,{
+		x : -1.5*fs,
+		rotation:'30deg',
+	},0)
+
+	.to(zsi,.3,{
+		y:-1.5*fs,
+		ease: Circ.easeOut,
+	},'-=.1')
+	.to(zsi,.3,{
+		y:0,
+		ease: Circ.easeIn,
+	})
+
+	.to('#zsi_fl',.2,{
+		x : 0*fs,
+		rotation:'0deg',
+	})
+	.to('#zsi_fr',.2,{
+		x : 0,
+		rotation:'0deg',
+	},'-=.2')
+
+	.to(zsi,.3,{
+		y:-1.5*fs,
+		ease: Circ.easeOut,
+	},'-=.1')
+	.to(zsi,.3,{
+		y:0,
+		ease: Circ.easeIn,
+	});
+	t5.repeat(2);
+
+	// 猪弟弟跳跃,时长1.4s
+	t6.to('#zbo_fl',.2,{
+		x : .5*fs,
+		rotation:'-30deg',
+		
+	})
+	.to('#zbo_fr',.2,{
+		x : -.6*fs,
+		rotation:'30deg',
+	},0)
+
+	.to(zbo,.3,{
+		y:-1.5*fs,
+		ease: Circ.easeOut,
+	},'-=.1')
+	.to(zbo,.3,{
+		y:0,
+		ease: Circ.easeIn,
+	})
+
+	.to('#zbo_fl',.2,{
+		x : 0*fs,
+		rotation:'0deg',
+	})
+	.to('#zbo_fr',.2,{
+		x : 0,
+		rotation:'0deg',
+	},'-=.2')
+
+	.to(zbo,.3,{
+		y:-1.5*fs,
+		ease: Circ.easeOut,
+	},'-=.1')
+	.to(zbo,.3,{
+		y:0,
+		ease: Circ.easeIn,
+	});
+	t6.repeat(2);
+
+	t1.repeat(-1);
+	t3.repeat(1);
+	t4.repeat(1);
+
+});
+
+
 
 homeStart.on('click', function() {
 	// console.log(zba);
