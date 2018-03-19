63d449d40de793a9a3910dc763db4483bbeda719
John <601473400@qq.com>
2018-03-19 21:54:52 +0800

    3-19

--- 小猪佩奇/js/index.js
+++ 小猪佩奇/js/index.js
@@ -838,12 +838,13 @@ function zbaWishStart() {
 
 }
 
-zbaWishGo();
+// zbaWishGo();
 // 猪爸愿望实现
 function zbaWishGo() {
 	var w4Zsi = $('.w4_zsi');
 	var w4Zbo = $('.w4_zbo');
 	var zbaWish = $('#zbaWish');
+	var wishPage = $('#wishPage');
 
 	var t = new TimelineMax();
 	var t1 = new TimelineMax();
@@ -875,12 +876,150 @@ function zbaWishGo() {
 		ease: Circ.easeOut,
 	})
 	t2.repeat(-1);
-	// t2.repeatDelay(1);
 
+	// 跳到下一页
 	t3.to(zbaWish,0,{
 		display: 'none',
 		onComplete: function() {
-			
+			t1.stop();
+			t2.stop();
+			wishPage.css('display','none');			
+			shakeShake();
 		}
 	},'+=4')
+}
+
+// 是否第一次摇
+var firstShake = false;	
+
+// shakeShake();
+// 摇一摇页面
+function shakeShake() {
+
+	var shakePage = $('#shakePage');
+	shakePage.css('display','block');
+	var yaoyiyao = $('.yaoyiyao');
+	var bg8_yyy = $('.bg8_yyy');
+	var bg10_yyy = $('.bg10_yyy');
+	if(firstShake) {
+		bg8_yyy.css('display','block');
+		bg10_yyy.css('display','none');
+		// firstShake = false;	
+	}else {
+		bg8_yyy.css('display','none');
+		bg10_yyy.css('display','block');
+	}
+	// 水泡
+	var q1 = $('.bg8_q1');
+	var q2 = $('.bg8_q2');
+	var q3 = $('.bg8_q3');
+
+	var t = new TimelineMax();
+	var t1 = new TimelineMax();
+	// 摇手机
+	t.to(yaoyiyao,.5,{rotation: 10})
+	.to(yaoyiyao,.5,{rotation: 0});
+	t.repeat(-1);
+	// 气泡上升
+	// t1.to(q3,2,{y: -1*fs,ease: Linear.easeIn})
+	// .to(q2,2,{y: -1*fs,ease: Linear.easeIn},0)
+	// .to(q1,2,{y: -1*fs,ease: Linear.easeIn},0)
+	// 气泡消失
+	// .to(q3,.5,{opacity: 0},'-=1.5')
+	// .to(q2,.5,{opacity: 0},'-=1')
+	// .to(q1,.5,{opacity: 0},'-=0.5')
+	// t1.repeat(-1);
+	if(window.DeviceMotionEvent){//判断设备是否支持运动传感事件。
+	        window.addEventListener('devicemotion', shake, false);//如果支持，那么就绑定shake方法到事件上
+	}else{
+	        alert('本设备不支持摇一摇功能');
+	}
+	//首先定义一下，全局变量
+	var lastTime = 0;//此变量用来记录上次摇动的时间
+	var x = y = z = lastX = lastY = lastZ = 0;//此组变量分别记录对应x、y、z三轴的数值和上次的数值
+	var shakeSpeed = 1000;//设置阈值
+	//编写摇一摇方法
+	function shake(eventData){
+	        var acceleration = eventData.accelerationIncludingGravity;//获取设备加速度信息
+	        var nowTime = new Date().getTime();//记录当前时间
+	        //如果这次摇的时间距离上次摇的时间有一定间隔 才执行
+	        if(nowTime - lastTime > 100){
+	                var diffTime = nowTime - lastTime;//记录时间段
+	                lastTime = nowTime;//记录本次摇动时间，为下次计算摇动时间做准备
+	                x = acceleration.x;//获取x轴数值，x轴为垂直于北轴，向东为正
+	                y = acceleration.y;//获取y轴数值，y轴向正北为正
+	                z = acceleration.z;//获取z轴数值，z轴垂直于地面，向上为正
+	                //计算 公式的意思是 单位时间内运动的路程，即为我们想要的速度
+	                var speed = Math.abs(x + y + z - lastX - lastY - lastZ) / diffTime * 10000;
+	                if(speed > shakeSpeed){//如果计算出来的速度超过了阈值，那么就算作用户成功摇一摇
+	                        t.stop();
+	                        shakePage.css('display','none');
+	                        shakeResult();
+	                        //这里就是放置如果用户成功的摇一摇，将要触发的事件
+	                }
+	                lastX = x;//赋值，为下一次计算做准备
+	                lastY = y;//赋值，为下一次计算做准备
+	                lastZ = z;//赋值，为下一次计算做准备
+	        }
+	}
+
+}
+
+shakeResult();
+// 摇一摇
+function shakeResult() {
+	// 是否第一次摇
+	if(firstShake){
+		firstShake = false;
+		var wishOne = $('#wishOne');
+		var bg9Btn = $('#bg9_btn');
+
+		wishOne.css('display','block');
+
+		var t = new TimelineMax();
+		t.fromTo(bg9Btn,1,{
+			y: 10*fs,
+		},{
+			y: 0,
+			ease: Back.easeOut,
+		});
+		bg9Btn.on('click',function() {
+			wishOne.css('display','none');
+			shakeShake();
+		})		
+	}else {
+		var wishTwo = $('#wishTwo');
+		var wishTwo2 = $('#wishTwo2');
+		var ticketBtn = $('#ticket_btn');
+
+		wishTwo.css('display','block');
+
+		ticketBtn.on('click',function() {
+			wishTwo.css('display','none');
+			wishTwo2.css('display','block');
+			toLastPage();
+		})
+
+	}
+
+}
+
+function toLastPage() {
+	var bg9Btn = $('#bg9_btn');
+	var lastPage = $('#lastPage');
+	bg9Btn.on('click',function() {
+		lastPage.css('display','block');
+		restart();
+	})
+}
+
+// 最后一页的点击
+function restart() {
+	var lastPage = $('#lastPage');
+	var playAgain= $('#playAgain');
+
+	playAgain.on('click',function() {
+		lastPage.css('display','none');
+		
+	})
 }
