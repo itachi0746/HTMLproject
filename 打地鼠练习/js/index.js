9cb3f92c3dc6d0a37d2adfff5921cb62a399c5b5
John <601473400@qq.com>
2018-03-24 23:22:16 +0800

    3-24

--- 打地鼠练习/js/index.js
+++ 打地鼠练习/js/index.js
@@ -1,6 +1,6 @@
 var hole = document.getElementsByClassName('wp')[0].children;
 // hole =  hole.getElementsByTagName('div');
-// console.log(hole.length);
+// console.log(hole);
 
 // 大狼
 var bigWolf = ['img/h0.png','img/h1.png','img/h2.png','img/h3.png','img/h4.png',
@@ -20,7 +20,7 @@ function select() {
 	var num = Math.floor(Math.random()*hole.length);
 	var temp = hole[num].children.length;
 	// console.log(temp!=0);
-	if(temp!=0) {
+	if(temp!==0) {
 		select();
 	}
 	else{
@@ -30,30 +30,48 @@ function select() {
 	}
 }
 
-// 狼出现
-function show(ele,tgt,index) {
+// 狼出现(元素,目标数组,下标)
+function show(ele,tgt,_index) {
 	var _img = document.createElement('img');
-	ele.append(_img);
-	// console.log(hole[0].children.length);
+	ele.appendChild(_img);
+	// console.log(ele);
 
 	var timer = setInterval(function() {
 		// show();
-		// console.log(index);
-		_img.src = tgt[index];
-		index++;
-		console.log(index);
+		// console.log(ele);
+		_img.src = tgt[_index];
+		_index++;
+		// console.log(__index);
 		// 敲打前的图数
-		if(index>=6) {
+		if(_index>=6) {
 			clearInterval(timer);
 			// ele.innerHTML = '';
-			// index = 0;
+			// _index = 0;
+			setTimeout(function(){
+                hide(ele,tgt,_index,_img);
+			},1000);
+
 		}
 
 	},80)
 }
 // console.log(hole);
 select();
-select();
-select();
 
+
+function hide(ele,tgt,_index,img) {
+	console.log(ele,tgt,_index);
+    _index--;
+	var timer = setInterval(function() {
+        _index--;
+		console.log(ele);
+        if(_index<0) {
+        	clearInterval(timer);
+        	ele.innerHTML = '';
+        	return;
+		}
+        img.src = tgt[_index];
+
+	},80)
+}
 
