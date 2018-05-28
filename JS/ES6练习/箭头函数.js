978daf99c9100afecee485c613f1769f3e1b255c
John <601473400@qq.com>
2018-05-28 18:00:09 +0800

    es6练习

---
+++ JS/ES6练习/箭头函数.js
@@ -0,0 +1,28 @@
+// var arr = [10, 20, 1, 2];
+
+
+
+// console.log(arr.sort((x,y) => y-x));
+var fn = x => x * x;
+console.log(fn(2));
+
+
+
+var obj = {
+  birth: 1990,
+  getAge: function () {
+      var b = this.birth; // 1990
+      var fn = () => new Date().getFullYear() - this.birth; // this指向obj对象
+      return fn();
+  }
+};
+var a = obj.getAge(); // 25
+console.log(a);
+
+const full = ({ first, last }) => first + ' ' + last;
+
+// 等同于
+function full(person) {
+  return person.first + ' ' + person.last;
+}
+
