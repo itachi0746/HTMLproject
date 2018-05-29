bf2f90d6556c6a64393840e883ee28e870c586cd
John <601473400@qq.com>
2018-05-29 23:38:12 +0800

    es6

--- JS/ES6练习/箭头函数.js
+++ JS/ES6练习/箭头函数.js
@@ -6,7 +6,7 @@
 
 // console.log(arr.sort((x,y) => y-x));
 var fn = x => x * x;
-console.log(fn(2));
+// console.log(fn(2));
 
 
 
@@ -19,12 +19,32 @@ var obj = {
   }
 };
 var a = obj.getAge(); // 25
-console.log(a);
+// console.log(a);
 
-const full = ({ first, last }) => first + ' ' + last;
+// const full = ({ first, last }) => first + ' ' + last;
 
-// 等同于
-function full(person) {
-  return person.first + ' ' + person.last;
+// // 等同于
+// function full(person) {
+//   return person.first + ' ' + person.last;
+// }
+
+function foo() {
+  setTimeout(() => {
+    console.log("id:", this.id);
+  }, 100);
 }
 
+var id = 21;
+
+// foo.call({ id: 42 });
+
+// ES5
+function foo2() {
+  var _this = this;
+
+  setTimeout(function () {
+    console.log('id:', _this.id);
+  }, 100);
+}
+
+foo2.call({ id: 42 });
