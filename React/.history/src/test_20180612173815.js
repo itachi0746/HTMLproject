2497b40939148493a1fdbce918e76cdc275e25d5
John <601473400@qq.com>
2018-06-12 18:26:05 +0800

    learn react

---
+++ React/.history/src/test_20180612173815.js
@@ -0,0 +1,31 @@
+
+import React, { Component } from 'react';
+
+class Toggle extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {isToggleOn: true};
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return(
+        <button onclick={this.handleClick}>
+          {this.state.isToggleOn?'ON':'OFF'}
+        </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
---
+++ React/.history/test_20180612151903.js
@@ -0,0 +1,12 @@
+/* jshint esversion: 6 */ 
+import React from 'react';
+
+function Welcome(props) {
+  return <h1>
+    
+  </h1>
+}
+
+const element = <Welcome name='Sara' />;
+
+ReactDOM.render(element, document.getElementById("root"));
