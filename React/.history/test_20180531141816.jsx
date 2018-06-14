79801d43287ad4be12262a45293e196680b05ca0
John <601473400@qq.com>
2018-06-14 18:23:38 +0800

    react-app

--- React/.history/test_20180531141816.jsx
+++
@@ -1,9 +0,0 @@
-
-
-function Welcome(props) {
-  return <h1>hello, {props.name}</h1> <h1></h1>
-}
-
-const element = <Welcome name='Sara' />;
-
-ReactDOM.render(element, document.getElementById("root"));
