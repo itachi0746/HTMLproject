2c2dc3c4e8ca3da39830d0daa4fa848704b14b95
John <601473400@qq.com>
2018-07-20 18:16:54 +0800

    ADD:滚动网页,css重置

---
+++ 特效/商城分类/static/js/classify.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  var myScroll2 = new IScroll('#tagBoxWrap', { click: true });
+  var myScroll = new IScroll('#contentBoxWrap', { click: true });
+
+  $('#tagBox li').on('click', function() {
+    $('#tagBox')
+      .find('li')
+      .each(function() {
+        $(this).removeClass('chosen');
+      });
+
+    $(this).addClass('chosen');
+  });
+});
