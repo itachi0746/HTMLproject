e2dae0566f04a0236b7954467297db5ecd536417
John <601473400@qq.com>
2018-07-25 18:27:36 +0800

    ADD:相册效果

--- 特效/商城分类/static/js/classify.js
+++ 特效/商城分类/static/js/classify.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
+(function() {
   var myScroll2 = new IScroll('#tagBoxWrap', { click: true });
   var myScroll = new IScroll('#contentBoxWrap', { click: true });
 
-  $('#tagBox li').on('click', function() {
-    $('#tagBox')
-      .find('li')
-      .each(function() {
-        $(this).removeClass('chosen');
-      });
+  // $('#tagBox li').on('click', function() {
+  //   $('#tagBox')
+  //     .find('li')
+  //     .each(function() {
+  //       $(this).removeClass('chosen');
+  //     });
 
-    $(this).addClass('chosen');
-  });
-});
+  //   $(this).addClass('chosen');
+  // });
+})();
