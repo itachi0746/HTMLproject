68e4e58dec990752df76e0333a550fed7f90459f
John <601473400@qq.com>
2019-02-20 13:56:30 +0800

    webpack多页面工具

---
+++ webpack多页面工具/src/pages/contact/contact.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import '../../common/css/com.scss';
+import './contact.css';
+import {throttle} from '../../tools/utils';
+import * as _ from "lodash";
+console.log("At page 'contact' :", _);
+console.log(throttle);
+
+console.log('22222');
---
+++ webpack多页面工具/src/pages/home/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import '../../common/css/com.scss';
+import './test.scss';
+import {throttle} from '../../tools/utils';
+import * as _ from "lodash";
+console.log("At page 'index' :", _);
+console.log(throttle);
+console.log('首页的js运行了～～11')
