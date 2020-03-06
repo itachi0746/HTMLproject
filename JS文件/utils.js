6832f03aa028c823af6df01f0f358294c480f525
John <601473400@qq.com>
2020-03-06 13:41:25 +0800

    增加js方法

--- JS文件/utils.js
+++ JS文件/utils.js
@@ -255,3 +255,13 @@ function hasSetRem(cb) {
     },300);
   }
 }
+
+// js将秒转化为时分秒
+function formatSeconds(value) {
+    let result = parseInt(value)
+    let h = Math.floor(result / 3600) < 10 ? '0' + Math.floor(result / 3600) : Math.floor(result / 3600)
+    let m = Math.floor((result / 60 % 60)) < 10 ? '0' + Math.floor((result / 60 % 60)) : Math.floor((result / 60 % 60))
+    let s = Math.floor((result % 60)) < 10 ? '0' + Math.floor((result % 60)) : Math.floor((result % 60))
+    result = `${h}:${m}:${s}`
+    return result
+}
